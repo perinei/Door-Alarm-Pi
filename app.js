4ea4d7e288cce5c8c1d444680990854b4f0be395
Perinei <adilson.perinei@yahoo.com>
2019-03-20 21:18:39 -0400

    Update app.js
    end of the day

--- app.js
+++ app.js
@@ -1,94 +1,69 @@
+var version = 6;
+var env = require('./env');
+console.log(env.sns_arn);
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
-// var dynamodb = new AWS.DynamoDB();
 var docClient = new AWS.DynamoDB.DocumentClient();
-
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED = new Gpio(26, 'out'); //use GPIO pin 26 as output
 var pushButton = new Gpio(13, 'in', 'both'); //use GPIO pin 13 as input, and 'both' button presses, and releases should be handled
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
-console.log('Alarm is on');
-// var fs = require('fs');
+var globalSerial;
+fMain();
 
-// // get serial
-
-// const exec = require( 'child_process' ).exec;
-// exec('cat /proc/cpuinfo | grep Serial',(error,stdout,stderr) => {
-//     if(error){
-//         console.error( `exec error: ${error}` );
-//         return;
-//     }
-
-// var serial = stdout.split(":");
-// serial = serial[1].trim();
-
-//     console.log( `stdout: ${stdout}` );// this is your RPI serial number
-//     console.log( `stderr: ${stderr}` );
-
-// console.log(`(${serial})`);
-
-// // create a file
-
-// // include node fs module
-
- 
-// // writeFile function with filename, content and callback function
-// fs.writeFile('serial.txt', serial, function (err) {
-//   if (err) throw err;
-//   console.log('File is created successfully.');
-// }); 
-
-// // create a file end //
-
-
-
-// });
-
-// // get serial end
-
-
-// fs.readFile('serial.txt', 'utf8', function(err, contents) {
-//   if (err) throw err;
-//   console.log(contents);
-  
-// });
-// console.log('after calling readFile');
-
-
-var ButtonStatus = pushButton.readSync();
-if (ButtonStatus == 0) {
-  LED.write(1);
-  console.log(ButtonStatus);
-  // console.log(serial);
-  writeToDynamoDB();
-  sendMessage("Open");
+async function fMain() {
+  try {
+    console.log(`Alarm service is UP - version:${version}`);
+    globalSerial = await fnSerial();
+    var ButtonStatus = pushButton.readSync();
+    var status;
+    if (ButtonStatus == 0) {
+      LED.write(1);
+      status = "Door is Open"
+    } else {
+      LED.write(0);
+      status = "Door is Closed"
+    }
+      writeToDynamoDB(status);
+      sendMessage(status);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-pushButton.watch(function (err, value) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
+pushButton.watch(async function (err, value) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
   if (err) { //if an error
     console.error('There was an error', err); //output error message to console
   return;
   }
-  var alarm = 0;
+  var status;
   if (value == 1) {
-    alarm = 0;
-    sendMessage("Close");
-    writeToDynamoDB("Close");
+    LED.write(0);
+    status = "Closed"
   } else
   {
-    alarm = 1;
-    console.log("writting to DB");
-    writeToDynamoDB("Open");
-    sendMessage("Open");
-    console.log("done writting to DB");
+    LED.write(1);
+    status = "Opened"
   }
-  LED.writeSync(alarm); //turn LED on or off depending on the button state (0 or 1)
-  console.log(alarm);
+  writeToDynamoDB(status);
+  sendMessage(status);
+  
+  LED.writeSync(value); //turn LED on or off depending on the button state (0 or 1)
 });
 
+async function fnSerial() {
+  const { stdout, stderr } = await exec('cat /proc/cpuinfo | grep Serial');
+  // console.log('stdout:', stdout);
+  // console.log('stderr:', stderr);
+  var serialSplit = stdout.split(":");
+  var serialOnly = serialSplit[1].trim();
+  // console.log(serial1);
+  return serialOnly;
+}
 
 function writeToDynamoDB(status) {
-  var serial = serial;
   var d = new Date();
   var seconds = Math.round(d.getTime() / 1000);
   
@@ -96,7 +71,7 @@ function writeToDynamoDB(status) {
     Item: {
      "date_time": seconds,
      "Status": status, 
-     "Serial": "16464546464"
+     "Serial": globalSerial
     },     
     ReturnConsumedCapacity: "TOTAL", 
     TableName: "office"
@@ -104,10 +79,30 @@ function writeToDynamoDB(status) {
 
    docClient.put(params, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-  
-  });
+    else     console.log(data);           // successful response  
+ });
+}
 
+function sendMessage(status) {
+    // Create publish parameters
+    var d = new Date();
+    var params = {
+    Message: `${globalSerial}:Office door:${status} on ${d}`,  /* required */
+    TopicArn: env.sns_arn
+  };
+
+  // Create promise and SNS service object
+  var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
+
+  // Handle promise's fulfilled/rejected states
+  publishTextPromise.then(
+    function(data) {
+      console.log(`Message ${params.Message} send sent to the topic ${params.TopicArn}`);
+      console.log("MessageID is " + data.MessageId);
+    }).catch(
+    function(err) {
+      console.error(err, err.stack);
+  });
 }
 
 function unexportOnClose() { //function to run when exiting program
@@ -116,25 +111,4 @@ function unexportOnClose() { //function to run when exiting program
   pushButton.unexport(); // Unexport Button GPIO to free resources
 };
 
-function sendMessage(status) {
-// Create publish parameters
-var params = {
-  Message: `Office door:${status}`,  /* required */
-  TopicArn: 'arn:aws:sns:us-east-1:286771205773:officedoor'
-};
-
-// Create promise and SNS service object
-var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
-
-// Handle promise's fulfilled/rejected states
-publishTextPromise.then(
-  function(data) {
-    console.log("Message ${params.Message} send sent to the topic ${params.TopicArn}");
-    console.log("MessageID is " + data.MessageId);
-  }).catch(
-    function(err) {
-    console.error(err, err.stack);
-  });
-}
-
-process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
+process.on('SIGINT', unexportOnClose); //run when user closes using ctrl+c
