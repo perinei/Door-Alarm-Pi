a2b513285642e92b1480aa9d2f8b7616d90595cc
Perinei <adilson.perinei@yahoo.com>
2019-03-25 12:36:05 -0400

    Update AfterInstall.sh

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -10,4 +10,6 @@ echo "module installed"
 echo "getteing Parameters store variables"
 sns_arn=$(aws ssm get-parameters --region us-east-1 --names sns_arn --query Parameters[0].Value)
 cd /opt/office/
-echo $sns_arn >> env.js
+rm env.js
+echo module.exports.sns_arn = $sns_arn >> env.js
+echo "Parameters stores saved to env.js"
