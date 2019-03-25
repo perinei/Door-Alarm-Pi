4b02ff538af2506930b278ca0d989bedde65d8dd
Perinei <adilson.perinei@yahoo.com>
2019-03-25 12:32:35 -0400

    Update AfterInstall.sh

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -4,4 +4,10 @@
 #sudo apt-get upgrade -y
 #cd /opt/M220/
 #npm install
+
 echo "module installed"
+
+echo "getteing Parameters store variables"
+sns_arn=$(aws ssm get-parameters --region us-east-1 --names sns_arn --query Parameters[0].Value)
+cd /opt/office/
+echo $sns_arn >> env.js
