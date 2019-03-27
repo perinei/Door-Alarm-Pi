97ba1fccada518a8bcac917872e2ecb79910913e
Perinei <zumiki@gmail.com>
2019-03-27 13:20:43 -0400

    Update AfterInstall.sh

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -8,12 +8,12 @@ cd /opt/doorSensor/
 npm install
 echo "modules installed"
 
-echo "getteing Parameters store variables"
-sns_arn=$(aws ssm get-parameters --names sns_arn --query Parameters[0].Value)
-echo $sns_arn
-#echo "deleting old env.js"
-cd /opt/doorSensor
+# echo "getteing Parameters store variables"
+# sns_arn=$(aws ssm get-parameters --names sns_arn --query Parameters[0].Value)
+# echo $sns_arn
+# echo "deleting old env.js"
+# cd /opt/doorSensor
 # rm env.js
-#creating new env.js with sns_arn
-echo module.exports.sns_arn = $sns_arn >> /opt/doorSensor/env.js
-echo "Parameters stores saved to env.js"
+# creating new env.js with sns_arn
+# echo module.exports.sns_arn = $sns_arn >> /opt/doorSensor/env.js
+# echo "Parameters stores saved to env.js"
