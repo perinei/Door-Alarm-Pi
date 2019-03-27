e0cf66814cf6f5d23f76836635de3eb2e71fa000
Perinei <zumiki@gmail.com>
2019-03-27 16:36:33 -0400

    Update ps.js

--- app/ps.js
+++ app/ps.js
@@ -2,15 +2,7 @@ var AWS = require('aws-sdk');
 var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
 AWS.config.credentials = credentials;
 AWS.config.update({region: 'us-east-1'});
-
 const awsParamStore = require( 'aws-param-store' );
 
-const awsParamStore = require( 'aws-param-store' );
- 
-let parameter = awsParamStore.getParameterSync( '/doorSensor/arn_sns');
-
+let parameter = awsParamStore.getParameterSync( '/doorSensor/arn_sns', {region: 'us-east-1'});
 console.log(parameter);
-
-
- 
-// Parameter info object for '/project1/my-parameter'
