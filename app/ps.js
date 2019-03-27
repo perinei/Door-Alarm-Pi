c5dbffb0c1a4933ba83ecc6380c4607f80b70608
Perinei <zumiki@gmail.com>
2019-03-27 15:50:28 -0400

    Create ps.js

---
+++ app/ps.js
@@ -0,0 +1,12 @@
+var AWS = require('aws-sdk');
+var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+AWS.config.credentials = credentials;
+AWS.config.update({region: 'us-east-1'});
+
+const awsParamStore = require( 'aws-param-store' );
+
+awsParamStore.getParametersByPath( 'arn_sns' )
+    .then( (parameters) => {
+
+        console.log(parameters);
+    });
