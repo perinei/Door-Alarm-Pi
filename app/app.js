b174a4db7a172430cf792f565df2d34c6a1200e9
Perinei <zumiki@gmail.com>
2019-03-28 14:38:36 -0400

    Update app.js

--- app/app.js
+++ app/app.js
@@ -79,7 +79,7 @@ async function fnSerial() { // return Pi Serial Number
 }
 
 async function fnRegion() {  // return AWS region 
-  const { stdout, stderr } = await exec('cat /home/perinei/.aws/config | grep region');
+  const { stdout, stderr } = await exec('cat ~/.aws/config | grep region');
   // console.log('stdout:', stdout);
   // console.log('stderr:', stderr);
   var splited = stdout.split("=");
