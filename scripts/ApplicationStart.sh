c7c7da99e87b04f162630abfafb7b79e65e1ce69
Perinei <adilson.perinei@yahoo.com>
2019-04-03 21:59:16 -0400

    scripts

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -3,6 +3,6 @@
 
 cd /home/app/doorSensor/
 #####      Creating a service call doorSensor
-forever-service install doorSensor -r app
+sudo forever-service install doorSensor -r app
 ##### start a service called doorSensor
-service doorSensor start
+sudo service doorSensor start
